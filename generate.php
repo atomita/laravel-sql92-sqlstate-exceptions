7ed0d125e01cf9625ea2d1df6e984e0769bd2568
atomita <sleeping.cait.sith+gh@gmail.com>
2022-01-06 03:33:18 +0900

    feat: Add generate script

---
+++ generate.php
@@ -0,0 +1,57 @@
+<?php
+
+$sqlstate = json_decode(file_get_contents(__DIR__ . '/sql92_sqlstate.json'), true);
+
+$output = __DIR__ . '/src/Exceptions';
+
+function studly(string $value)
+{
+    return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', strtolower($value))));
+}
+
+function endsWith(string $haystack, string $needle)
+{
+    return substr($haystack, -strlen($needle)) === $needle;
+}
+
+function isSubclass(string $code): bool
+{
+    return substr($code, -3) !== '000';
+}
+
+function isNoSubclass(string $value): bool
+{
+    return endsWith($value, '_NO_SUBCLASS');
+}
+
+function stripNoSubclass(string $value): string
+{
+    return isNoSubclass($value) ? substr($value, 0, -12) : $value;
+}
+
+function mainClass(string $code): string
+{
+    return substr($code, 0, -3) . '000';
+}
+
+function make(string $class, string $parent): string
+{
+    return <<<PHP
+<?php
+namespace atomita\LaravelSql92Exceptions\Exceptions;
+
+class $class extends $parent
+{
+}
+PHP;
+}
+
+unset($sqlstate['SQLSTATE 00000']);
+foreach ($sqlstate as $code => $name) {
+    $class = studly(stripNoSubclass($name)) . 'Exception';
+
+    $parentName = isSubclass($code) ? $sqlstate[mainClass($code)] ?? null : null;
+    $parent = empty($parentName) ? '\Illuminate\Database\QueryException' : studly(stripNoSubclass($parentName)) . 'Exception';
+
+    file_put_contents("{$output}/{$class}.php", make($class, $parent));
+}
